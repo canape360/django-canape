e765990e4e1cc9b60314ba3edda13fba31e28188
mich360 <fghbg751@yahoo.co.jp>
2026-01-17 16:35:07 +0900

    Fix Python path for Vercel (add src to sys.path)

--- src/main/wsgi.py
+++ src/main/wsgi.py
@@ -2,8 +2,10 @@ import os
 import sys
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parent.parent
-sys.path.append(str(BASE_DIR))
+# ===== ここ超重要 =====
+BASE_DIR = Path(__file__).resolve().parent.parent  # /var/task/src
+sys.path.insert(0, str(BASE_DIR))
+# ======================
 
 from django.core.wsgi import get_wsgi_application
 
