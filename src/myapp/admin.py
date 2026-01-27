5108c9af6319351b86b6ad93b7c45a3a1d08b81c
mich360 <fghbg751@yahoo.co.jp>
2026-01-27 12:21:36 +0900

    Fix admin list_display: author -> user

--- src/myapp/admin.py
+++ src/myapp/admin.py
@@ -2,13 +2,10 @@ from django.contrib import admin
 from .models import MyApp, Person, MyMail
 
 
-# MyApp 用の admin クラス
 class MyAppAdmin(admin.ModelAdmin):
-    # author → user に修正
     list_display = ("title", "created_at", "user")
 
 
-# モデルを admin に登録
 admin.site.register(MyApp, MyAppAdmin)
 admin.site.register(Person)
 admin.site.register(MyMail)
