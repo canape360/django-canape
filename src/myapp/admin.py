1108d10cfb76d781a02835da8caffe3e5035c1ee
mich360 <fghbg751@yahoo.co.jp>
2026-01-27 12:16:18 +0900

    Fix admin: replace author with user

--- src/myapp/admin.py
+++ src/myapp/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from .models import MyApp, Person, MyMail
 
+
 # MyApp 用の admin クラス
 class MyAppAdmin(admin.ModelAdmin):
-    list_display = ('title', 'created_at', 'author')  # timestamp → created_at に変更し、作成者も表示
+    # author → user に修正
+    list_display = ("title", "created_at", "user")
+
 
 # モデルを admin に登録
 admin.site.register(MyApp, MyAppAdmin)
