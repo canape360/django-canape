f281eb6be303c52956b829fb95946566d899fe95
mich360 <fghbg751@yahoo.co.jp>
2026-01-27 13:31:26 +0900

    Make myapp 0007 a noop (managed=False metadata)

---
+++ src/myapp/migrations/0007_alter_myapp_options_alter_mymail_options_and_more.py
@@ -0,0 +1,8 @@
+from django.db import migrations
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("myapp", "0006_alter_myapp_options_alter_mymail_table"),
+    ]
+
+    operations = []
